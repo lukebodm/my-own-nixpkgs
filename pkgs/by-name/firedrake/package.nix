ebd905a98f7d014d740a1fe982fc4b3e79442bae
Luke Bodmer <lukebodm@buffalo.edu>
2024-04-29 13:15:04 -0400

    added firedrake

--- pkgs/by-name/firedrake/package.nix
+++ pkgs/by-name/firedrake/package.nix
@@ -1,46 +1,49 @@
-{ pkgs ? import <nixpkgs> {} }:
+# This just brings in everything.
+#
+# This isn't really informative or overridable in any practical sense.  Could put the closure of the dependencies
+# of all the packages declared here instead.  This would be overridable/informative, but a bigger pain to maintain.
+#
+# Likely this should be added into the python packages set instead of dropped on the top-level.
+#
+{ pkgs }:
 
-pkgs.stdenv.mkDerivation rec {
-  name = "firedrake-env";
+let
+  self =
+    let
+      # Top-level scope extended with this set of packages.
+      #
+      # Possibly it would make more sense to use the python-level scope instead?
+      #
+      callPackage = pkgs.newScope self;
 
-  src = pkgs.fetchurl{
-    url = "https://raw.githubusercontent.com/firedrakeproject/firedrake/master/scripts/firedrake-install";
-    sha256 = "sha256-HD8g4jQOM1MGm42LMY71zdJVsihvwY6+gnvH40SJzt0";# Replace this with the actual sha256 hash of the script
-  };
+    in
+      with self; {
+        inherit callPackages;
 
-  buildInputs = [
-     pkgs.python3
-     pkgs.python310Packages.setuptools
-     pkgs.python310Packages.packaging
-     pkgs.util-linux # for lscpu command
-     pkgs.gcc # A C and C++ compiler
-     pkgs.gcc10 # If gcc 10 specifically required
-     pkgs.gnumake # GNU make
-     pkgs.gfortran # A Fortran compiler
-     pkgs.openblas # Blas
-     pkgs.lapack # Lapack
-     pkgs.git # Git
-     pkgs.mercurial # Mercurial
-     pkgs.autoconf # autoconf
-     pkgs.automake # automake
-     pkgs.libtool # libtool
-     pkgs.cmake # CMake
-     pkgs.zlib # zlib
-     pkgs.flex # flex
-     pkgs.bison # bison
-     pkgs.pkg-config
-   ];  # Add any necessary dependencies
+        # Python to use for all this stuff (much of it is python)
+        python         = pkgs.python27;
+        pythonPackages = python.pkgs;
 
-  phases = ["buildPhase"];
+        # Forked and unavailable packages required by firedrake and its dependencies
+        ufl       = callPackage ./ufl.nix       { };
+        fiat      = callPackage ./fiat.nix      { };
+        finat     = callPackage ./finat.nix     { };
+        tsfc      = callPackage ./tsfc.nix      { };
+        pyop2     = callPackage ./pyop2.nix     { };
+        petsc     = callPackage ./petsc.nix     { };
+        petsc4py  = callPackage ./petsc4py.nix  { };
+        coffee    = callPackage ./coffee.nix    { };
+        firedrake = callPackage ./firedrake.nix { };
+        pulp      = callPackage ./pulp.nix      { };
+        sowing    = callPackage ./sowing.nix    { };
+        metis     = callPackage ./metis.nix     { };
+        hypre     = callPackage ./hypre.nix     { };
+        parmetis  = callPackage ./parmetis.nix  { };
+        exodus    = callPackage ./exodus.nix    { };
 
-  buildPhase = ''
-    echo "WORKING DIRECTORY:"
-    pwd
-    ls
-    mkdir -p $out/bin
-    cp $src $out/bin/firedrake-install
-    chmod +x $out/bin/firedrake-install
-    cd $out/bin
-    python3 firedrake-install --help
-  '';
-}
+        # Incompatible version (something with communicator/datatype cleanup callbacks?)
+        mpi4py    = callPackage ./mpi4py.nix { };
+      };
+
+in
+  self.firedrake
